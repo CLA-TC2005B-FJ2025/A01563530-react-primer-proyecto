8c9dba605834b888c553737e6343f204092aa632
Rocío Rodríguez <147364842+CherryChio@users.noreply.github.com>
2025-03-28 16:01:25 +0000

    Luigi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Tarjeta } from './Tarjeta'
 
+import MarioImage from './imagenes/Mario.png'
+import LuigiImage from './imagenes/Luigi.png'
+import PeachesImage from './imagenes/Peaches.png'
+
 export function App() {
   return (
     <div>
         <div>Personajes de Mario Bros</div>
-        <Tarjeta/>
-        <Tarjeta/>
-        <Tarjeta/>
+        <Tarjeta nombre="Mario" colorFavorito="Rojo" image={MarioImage}/>
+        <Tarjeta nombre="Luigi" colorFavorito="Verde" image={LuigiImage}/>
+        <Tarjeta nombre="Peaches" colorFavorito="Rosa" image={PeachesImage}/>
     </div>
   )
 }
